9acc3f6f0dcef93ff23da67814c2cc4c507a47b7
Sourav Singh <commitsovercoffee@gmail.com>
2023-06-30 12:53:14 +0530

    adds sveltekit and tailwind

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<section class="mx-auto my-16 rounded-xl bg-pink-100 prose prose-pink">
+	<p class="p-4 m-2">
+		Kickstart your web development journey with <a
+			href="https://kit.svelte.dev">SvelteKit</a
+		>
+		and
+		<a href="https://tailwindcss.com/docs/installation">Tailwind</a
+		>. Checkout the
+		<a href="https://github.com/commitsovercoffee/skeleton">repo</a>
+		for more such templates.
+	</p>
+</section>
